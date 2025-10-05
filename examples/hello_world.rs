45c7cacfa0a537bb1b0ac5f6072ad842bbb76c99
= <=>
2025-10-05 16:51:35 -0700

    Hello World Refactoring

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -3,7 +3,7 @@
 //! This is a simple example of how to use the `my_gui` framework to create a
 //! "Hello, World!" application.
 use env_logger;
-use my_gui::prelude::*;
+use my_gui::{core::render::objects::text_object::TextObject, prelude::*};
 
 // 1. Define the application state.
 pub struct App {
@@ -38,31 +38,24 @@ impl App {
     pub fn new() -> Self {
         let display_text = "日本語ハローワールドテスト。".to_string();
         let mut scene = Scene::new();
-        // scene.add_object(TextObject::new(&display_text, 10.0, 10.0));
-        // scene.add_object(Rectangle::new(10.0, 50.0, 200.0, 100.0));
-        // // Add a circle
-        // scene.add_object(Ellipse::new(300.0, 100.0, 50.0, 50.0));
-        // // Add a stretched ellipse
-        // scene.add_object(Ellipse::new(500.0, 100.0, 100.0, 30.0));
-        // // Add a line
-        // scene.add_object(Line::new_with_xy(10.0, 170.0, 600.0, 200.0, 2.0));
+        scene.add_object(TextObject::new(
+            display_text.clone(),
+            10.0,
+            10.0,
+            Color::GREEN,
+        ));
+        scene.add_object(Rectangle::new(10.0, 50.0, 200.0, 100.0, Color::WHITE));
+        // Add a circle
+        scene.add_object(Ellipse::new(300.0, 100.0, 50.0, 50.0, Color::WHITE));
+        // Add a stretched ellipse
+        scene.add_object(Ellipse::new(500.0, 100.0, 100.0, 30.0, Color::GREEN));
+        // Add a line
+        scene.add_object(Line::new(10.0, 170.0, 600.0, 200.0, 2.0, Color::GREEN));
 
         // Create a canvas
         let mut canvas = Canvas::new(250.0, 250.0, 300.0, 200.0);
-        canvas.add_object(Box::new(Rectangle::new(
-            10.0,
-            10.0,
-            100.0,
-            50.0,
-            Color::RED,
-        )));
-        canvas.add_object(Box::new(Ellipse::new(
-            150.0,
-            100.0,
-            50.0,
-            50.0,
-            Color::BLUE,
-        )));
+        canvas.add_object(Rectangle::new(10.0, 10.0, 100.0, 50.0, Color::RED));
+        canvas.add_object(Ellipse::new(150.0, 100.0, 50.0, 50.0, Color::BLUE));
         scene.add_object(canvas);
 
         Self {
